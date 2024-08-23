98562f9f25d6fc2cfca22dab45ecc4262b7740cd
cor22982 <cor22982@uvg.edu.gt>
2024-08-23 16:10:07 -0600

    Refactor Lab4

---
+++ labs/Laboratorio3/agents/linkeding_lookup_agent.py
@@ -0,0 +1,72 @@
+#Este es el ejercicio 3 de la clase de 9 de Agosto
+
+#Este es un ejemplo con agentes. Este es un programa al que le damos un nombre y un apellido
+#y gracias al uso de agentes y tools vamos a poder dejar que el LLM vaya  y busque el perfil
+#Y nos regrese el URL
+
+#funciones sistema operativo
+import os
+#variables de entorno
+from dotenv import load_dotenv
+load_dotenv()
+#ayuda a crear nuestro LLM
+from langchain_openai import ChatOpenAI
+#crea un template del prompt que se le manda al LLM
+from langchain.prompts.prompt import PromptTemplate
+from Tools.tools import get_profile_url_tavily
+#libreria para las tools y son funciones que communican al LLM con el exterior
+from langchain_core.tools import Tool
+#importar para los agents
+from langchain.agents import (
+    # y ese es ya un tipo de agente el react agent.
+    create_react_agent,
+    AgentExecutor, #este es un framework para correr.
+)
+# nos rive par aque podamos tomar diferentes prompts que ya estan hechos por la comunidad
+from langchain import hub
+
+def lookup (name: str) -> str:
+    llm = ChatOpenAI(
+        temperature=0,
+        model_name = "gpt-3.5-turbo",
+    )
+    #pedido a chatgpt que queremos
+    template = """
+        given the full name {name_of_pearson} I want you to get me a link to their Linkedin 
+        profile page. 
+            Your answer should contain only a URL."""
+    prompt_template = PromptTemplate(
+        template=template, #el template
+        input_variable=["name_of_pearson"] #la variable de entrada
+    )
+
+    #coontenedor con las tools que nuestro agente va a usar
+
+    tools_for_agent = [
+        Tool(
+            name = "Crawl Google 4 Linkedin profile page",
+            func = get_profile_url_tavily,
+            description = "useful when you need to get a Linkedin page URL",
+        )]
+    # otra parte que necesitan es una apllicacion de reazomamiento.
+    # para eso ya hay agents precreados
+    react_promt = hub.pull("hwchase17/react") #es el perfil de quien creo lanchain
+    #este es el agent
+    #con ese react prompt le diremos que use el react model
+    agent = create_react_agent(llm=llm, tools=tools_for_agent, prompt=react_promt)
+
+    #runtime para ejecutar el agente
+    #verbose es para visualizar el proceso de razonamiento
+    agent_executor = AgentExecutor(agent=agent,
+                                   tools=tools_for_agent,
+                                   verbose=True)
+    result = agent_executor.invoke(
+        input= {"input": prompt_template.format_prompt(name_of_pearson=name)}
+    )
+    linkedin_profile_url = result["output"]
+    return linkedin_profile_url
+
+
+if __name__ == "__main__":
+    linkedin_url = lookup(name="Mathew Cordero UVG Guatemala Tecnico Electronica")
+    print(linkedin_url)
---
+++ labs/Laboratorio4/main.py
@@ -0,0 +1,70 @@
+import os
+from dotenv import  load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_pinecone import PineconeVectorStore
+from langchain import hub
+from langchain.chains.combine_documents import create_stuff_documents_chain
+# es para ver el tema de retrival chains
+from langchain.chains.retrieval import  create_retrieval_chain
+from langchain_core.runnables import RunnablePassthrough
+
+load_dotenv()
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+if __name__ == '__main__':
+    print("Retrieving")
+    embeddings = OpenAIEmbeddings()
+    llm = ChatOpenAI()
+
+    #vamos a hacer una variable query y este query es la pregunta que le vamos a hacer a nuestro agente
+    query = "what is Pinecone in machine learning"
+
+    chain = PromptTemplate.from_template(template=query) | llm
+    result = chain.invoke(input={})
+    print(result.content)
+
+    #pinecone object
+    vectorStore = PineconeVectorStore(
+        index_name=os.environ["INDEX_NAME"],
+        embedding=embeddings
+    )
+
+    #prompt model de la comunidad para poder hacer el Q&A
+    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+
+    #chain que toma el contenido de los docuementos y los une
+
+    combine_docs_chain = create_stuff_documents_chain(llm, retrieval_qa_chat_prompt)
+
+    #obtener los documentos y vamos a unir todo el contenido
+
+    retrieval_chain = create_retrieval_chain(
+        retriever=vectorStore.as_retriever(),
+        combine_docs_chain=combine_docs_chain
+    )
+
+    result = retrieval_chain.invoke({"input": query})
+
+    print(result)
+
+    template = """ Use the following pieces of context to answer the question at the end
+    If you dont know the answer, just say you don't know, don't try to answer.
+    Use three sentences maximum and keep the answer as concise as posibble. 
+    Always say "tanks for asking" at the end of the answer.
+    {context}
+    Question: {question}
+    Helpful Answer: """
+
+    custom_rag_prompt = PromptTemplate.from_template(template)
+
+    rag_chain = (
+        {"context": vectorStore.as_retriever()|format_docs, "question":
+         RunnablePassthrough()} | custom_rag_prompt | llm
+    )
+    res = rag_chain.invoke(query)
+    print(res)
+
+
