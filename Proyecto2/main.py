7276f9451da1e088ad4638f28092ccd6180373ec
cor22982 <cor22982@uvg.edu.gt>
2024-11-24 16:17:39 -0600

    feat: Add all csv

--- Proyecto2/main.py
+++ Proyecto2/main.py
@@ -9,5 +9,16 @@ from langchain_openai import ChatOpenAI
 from langchain.agents import create_react_agent, AgentExecutor
 from langchain_experimental.tools import PythonREPLTool
 from langchain_experimental.agents.agent_toolkits import create_csv_agent
+import streamlit as st
+
 
 load_dotenv()
+
+def interfaz ():
+    st.set_page_config(page_title="Proyecto 2",
+                       layout= "wide")
+    st.title("Proyecto 2 ")
+
+
+if __name__=="__main__":
+    interfaz()
